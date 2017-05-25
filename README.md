96b19fb6861c45017d2219d27aa13eaccc11a8a3
Victor Sena Molero <victorsenam@gmail.com>
2017-05-24 22:02:50 -0300

    Maybe all of it

---
+++ README.md
@@ -0,0 +1,9 @@
+# What?
+This is a simple and not very careful script that fetches ACM-ICPC WF scoreboards and puts them on a table formatted for [vjudge's](https://vjudge.net/) replay contest feature. 
+
+# Instructions
+- Visit [some WF scoreboard](http://myicpc.icpcnews.com/World-Finals-2016/scoreboard) on myicpc live.
+- Run the script on `fetch.js` using developer tools
+- Copy that table to some [spreadsheet software](docs.google.com/spreadsheets)
+- Create a contest on [vjudge](https://vjudge.net) and select "Replay" on type
+- Upload the spreadsheet on "Replay Info"
---
+++ fetch.js
@@ -0,0 +1,24 @@
+var info= document.querySelectorAll('#mainScoreboard tbody tr');
+
+var res = "";
+for (var k = 0; k < info.length; k++) {
+  var el = info[k];
+  res += "<tr><td>" + el.children[2].children[0].innerHTML + "</td>";
+  for (var i = 6; i < el.children.length; i++) {
+    var curr = el.children[i].children;
+    var val;
+
+    if (curr.length == 0) {
+      val = " ";
+    } else if (curr[0].children.length == 0) {
+      val = curr[0].innerHTML+"#-";
+    } else {
+      val = curr[0].innerText.replace(" - ", "#");
+    }
+
+    res += "<td>"+val+"</td>"
+  }
+  res += "</tr>";
+}
+
+document.write("<table>"+res+"</table>");
